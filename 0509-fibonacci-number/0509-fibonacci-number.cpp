e4e0876ac36c9957bdfbeeb6d1787fa597de6711
Aditya Yadav <adityayadav767675@gmail.com>
2026-09-27 02:32:01 +0530

    Time: 13 ms (31.51%), Space: 7.8 MB (65.06%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,7 +4,9 @@ public:
         if(n<=1){
             return n;
         }
-        return fib(n-1)+fib(n-2);
+        int last = fib(n-1);
+        int slast = fib(n-2);
+        return last + slast;
 
     }
 };
